d941c57addb650f509e6278da0b209fc69c60617
Bundy01 <26124850+Bundy01@users.noreply.github.com>
2023-06-02 19:19:12 +0000

    Correction de install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ DEST="/usr/share/fortune/wisdom-fr"
 
 	if [ ! -d "$DEST" ]; then
 echo
-sudo mkdir -pm755 ${DEST}
+sudo install -dm755 ${DEST}
 printf "=> Création du répertoire \e[1mwisdom-fr\e[0m…\n"
 		fi
 	if [ -d "$DEST" ]; then
 echo
-sudo install -Dm644 Asie{,.dat}       ${DEST}
-sudo install -Dm644 Occident{,.dat}   ${DEST}
-sudo install -Dm644 RastAfrica{,.dat} ${DEST}
+sudo install -m644 Asie{,.dat}       ${DEST}
+sudo install -m644 Occident{,.dat}   ${DEST}
+sudo install -m644 RastAfrica{,.dat} ${DEST}
 printf "\033[3;32;3m:: L'installation des fichiers est réussie\033[0m\n"
 		fi
